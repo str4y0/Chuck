4f48e5ea99e91b5c0751c36b13c823bb34ace3d4
Chris Aleksejev <chris.aleksejev1@gmail.com>
2026-02-18 20:21:26 +0200

    Chuck

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
-const JokeV = document.querySelector('#JokeDisplay')
-const JokeBtn =document.querySelector('#Btn')
-
 async function getJoke() {
+  const JokeV = document.getElementById('JokeDisplay')
+  const JokeBtn =document.getElementById('Btn')
+  
   const response = await fetch('https://api.chucknorris.io/jokes/random');
   const Joke = await response.json();
   console.log(Joke.value);
   JokeV.textContent = Joke.value
+  JokeV.style = "text-align: center; font-size: 50px;background: lightgray; border: solid black; margin-left: 400px; margin-right:400px; border-radius: 25px;"
 }
-
-
-
-JokeBtn.addEventListener("click", getJoke);
